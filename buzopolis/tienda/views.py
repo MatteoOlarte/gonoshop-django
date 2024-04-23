bfaf209cce0ed06f1c9ff4740da8b5d8d9391a1c
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-04-22 20:53:50 -0500

    Creación de urls para la tienda

--- buzopolis/tienda/views.py
+++ buzopolis/tienda/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def pagina_inicio(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Pagina De Inicio')
+
+
+def pagina_inicio_redirect(request: HttpRequest) -> HttpResponse:
+    return redirect('tienda:inicio')
+
+
+def lista_productos(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Compra de productos')
+
+
+def detalle_producto(request: HttpRequest) -> HttpResponse:
+    return HttpResponse('Detalle Del Producto')
