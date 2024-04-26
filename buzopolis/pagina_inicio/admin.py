08175ad8996071f952c5f4bb2f0ddda62e1a9128
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-04-25 20:01:17 -0500

    Carrusel de imágenes al 100%
    Co-authored-by: luiis-21 <luiis-21@users.noreply.github.com>
    Co-authored-by: Santiago5711 <Santiago5711@users.noreply.github.com>

--- buzopolis/pagina_inicio/admin.py
+++ buzopolis/pagina_inicio/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import ImagenCarrusel
+
+
 # Register your models here.
+@admin.register(ImagenCarrusel)
+class ImagenCarruselAdmin(admin.ModelAdmin):
+    list_display = ['titulo', 'created', 'visible']
+    search_fields = ['created', 'visible']
+
--- buzopolis/pagina_inicio/views.py
+++ buzopolis/pagina_inicio/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render, redirect
 from django.http import HttpRequest, HttpResponse
 
+from .models import ImagenCarrusel
+
 
 # Create your views here.
 def pagina_inicio(request: HttpRequest) -> HttpResponse:
-    return render(request, 'pagina_inicio/index/index-template.html')
+    carrusel_img = ImagenCarrusel.objects.filter(visible=True)
+
+    context = {
+        'nombre_url': '',
+        'carrusel': carrusel_img
+    }
+    return render(request, 'pagina_inicio/index/index-template.html', context)
 
 
 def pagina_inicio_redirects(request: HttpRequest) -> HttpResponse:
