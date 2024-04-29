362a440554c554a6d03899c2e49d1ca8ad86e507
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-04-29 16:54:40 -0500

    Modulo de autenticacion 🔐

--- buzopolis/buzopolis/settings.py
+++ buzopolis/buzopolis/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
+    'autenticacion.apps.AutenticacionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
