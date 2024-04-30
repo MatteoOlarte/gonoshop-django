685f4388d4d39075fcb845f8ca98e022f7746a88
MatteoOlarte <115902090+MatteoOlarte@users.noreply.github.com>
2024-04-30 15:55:48 -0500

    agregando modelos al panel de administración

--- buzopolis/buzopolis_tienda/admin.py
+++ buzopolis/buzopolis_tienda/admin.py
@@ -1,9 +1,26 @@
 from django.contrib import admin
 
+from .models import ColorProducto
 from .models import Producto
+from .models import TipoProducto
 
 
 # Register your models here.
 @admin.register(Producto)
 class ProductoAdmin(admin.ModelAdmin):
-    ...
+    list_display = ['nombre', 'tipo_producto', 'color', 'talla', 'precio', 'stock', 'disponibilidad', 'publicado']
+    list_filter = ['tipo_producto', 'color', 'talla', 'precio', 'disponibilidad', 'stock', 'publicado']
+    search_fields = ['nombre', 'tipo_producto__nombre', 'color__nombre', 'description']
+    date_hierarchy = 'created'
+    raw_id_fields = ['color', 'tipo_producto']
+    prepopulated_fields = {'slug': ['nombre']}
+
+
+@admin.register(TipoProducto)
+class TipoProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
+
+
+@admin.register(ColorProducto)
+class ColorProductoAdmin(admin.ModelAdmin):
+    list_display = ['nombre']
--- buzopolis/buzopolis_tienda/models.py
+++ buzopolis/buzopolis_tienda/models.py
@@ -6,7 +6,19 @@ from django.urls import reverse
 
 # Create your models here.
 class TipoProducto(models.Model):
-    nombre = models.CharField(max_length=250)
+    nombre = models.CharField(max_length=250, verbose_name='Nombre Categoría')
+
+    @override
+    def __str__(self) -> str:
+        return self.nombre
+
+
+class ColorProducto(models.Model):
+    nombre = models.CharField(max_length=250, verbose_name='Nombre Color')
+
+    @override
+    def __str__(self) -> str:
+        return self.nombre
 
 
 class Producto(models.Model):
@@ -14,8 +26,8 @@ class Producto(models.Model):
         verbose_name = 'producto'
         verbose_name_plural = 'productos'
         indexes = [
-            models.Index(fields=['slug'], name='idx_slug_producto'),
-            models.Index(fields=['-precio'], name='idx_precio_producto')
+            models.Index(fields=['slug'], name='idx_producto_slug'),
+            models.Index(fields=['-precio'], name='idx_producto_precio')
         ]
         ordering = ['-precio']
 
@@ -31,12 +43,18 @@ class Producto(models.Model):
     slug = models.SlugField(max_length=250)
     description = models.TextField(max_length=2500)
     imagen = models.ImageField(upload_to='tienda/productos/%Y/%m/%d/%H')
-    talla = models.CharField(max_length=3, choices=Tallas.choices, default=Tallas.S)
+    talla = models.CharField(
+        max_length=3, choices=Tallas.choices, default=Tallas.S)
     precio = models.DecimalField(max_digits=20, decimal_places=2)
-    color = models.CharField(max_length=250)
     stock = models.IntegerField()
     disponibilidad = models.BooleanField(default=True, editable=False)
     publicado = models.BooleanField(default=False)
+    created = models.DateField(auto_now_add=True)
+    color = models.ForeignKey(
+        to=ColorProducto,
+        on_delete=models.RESTRICT,
+        related_name="+"
+    )
     tipo_producto = models.ForeignKey(
         to=TipoProducto,
         on_delete=models.RESTRICT,
@@ -52,11 +70,10 @@ class Producto(models.Model):
         Si el stock es 0, la disponibilidad se establece en False. De lo contrario,
         se establece en True.
         '''
-        self.disponibilidad = (self.stock == 0)
+        self.disponibilidad = (self.stock != 0)
         super(Producto, self).save(args, **kwargs)
 
-
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse('tienda:detalle_producto', args=[self.slug, self.id])
 
 
